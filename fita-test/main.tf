6e4a285a2392f317ed611286601f9abafcf3a62d
alief <aliefdarul5@gmail.com>
2022-08-30 09:44:46 +0700

    add provider

--- fita-test/main.tf
+++ fita-test/main.tf
@@ -7,4 +7,17 @@ terraform {
       name = "terraform-gcp-fita"
     }
   }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = "lunar-geography-261201"
+  region      = "asia-southeast1"
+  zone        = "asia-southeast1-a"
 }
