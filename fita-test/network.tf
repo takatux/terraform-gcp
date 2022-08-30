e20fbdf30445e2a308ff519d9e5a8eb2cc633c6d
alief <aliefdarul5@gmail.com>
2022-08-31 06:41:53 +0700

    create network

--- fita-test/network.tf
+++ fita-test/network.tf
@@ -1,5 +1,6 @@
 module "network" {
   source     = "../modules/network"
+  project_id = var.project_id
   env        = var.env
   company    = var.company
   private_cidr_range = "10.0.0.0/24"
--- modules/network/network.tf
+++ modules/network/network.tf
@@ -1,6 +1,6 @@
 resource "google_compute_network" "vpc_network" {
-  project                 = var.project_id
-  name                    = "${format("%s","${var.company}-${var.env}-vpc")}"
-  auto_create_subnetworks = false
-  routing_mode            = "GLOBAL"
+  project                     = var.project_id
+  vpc_name                    = "${format("%s","${var.company}-${var.env}-vpc")}"
+  auto_create_subnetworks     = false
+  routing_mode                = "GLOBAL"
 }
