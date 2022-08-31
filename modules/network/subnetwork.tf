dd31b706d2ec78f3cb179c318b00189887b115ea
alief <aliefdarul5@gmail.com>
2022-08-31 07:25:55 +0700

    create network

--- modules/network/subnetwork.tf
+++ modules/network/subnetwork.tf
@@ -1,12 +1,12 @@
 resource "google_compute_subnetwork" "private-subnet" {
-  name          = "${format("%s","${var.company}-${var.env}-private-subnet")}"
+  name          = "${format("%s","${var.company}-${var.env}-private-${var.region}")}"
   ip_cidr_range = var.private_cidr_range
   region        = var.region
   network       = google_compute_network.default.id
 }
 
 resource "google_compute_subnetwork" "public-subnet" {
-  name          = "${format("%s","${var.company}-${var.env}-public-subnet")}"
+  name          = "${format("%s","${var.company}-${var.env}-public-${var.region}")}"
   ip_cidr_range = var.public_cidr_range
   region        = var.region
   network       = google_compute_network.default.id
