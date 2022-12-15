050f9202dd746bc860383b744537fd37abb8e184
alief.satupintu <alief.ikhsan@satupintu.co.id>
2022-12-15 15:18:51 +0700

    fix userdata

--- opensuse-meetup-demo/instance.tf
+++ opensuse-meetup-demo/instance.tf
@@ -1,7 +1,3 @@
-data "template_file" "user_data" {
-  template = file("./cloud-init.yaml")
-}
-
 module "instance" {
   source          = "../modules/instance"
   instance_name   = "opensuse-instance"
