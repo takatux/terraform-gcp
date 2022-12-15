b8bee4c3c3708c413553f98d9bd0e7fddd65b6dd
alief.satupintu <alief.ikhsan@satupintu.co.id>
2022-12-15 16:33:54 +0700

    fix vars

--- opensuse-meetup-demo/instance.tf
+++ opensuse-meetup-demo/instance.tf
@@ -3,5 +3,7 @@ module "instance" {
   instance_name   = "opensuse-instance"
   image           = "opensuse-leap-15-4-v20221201-x86-64"
   tags            = var.tags
-  
+  region          = var.region
+  env             = var.env
+  company         = var.company        
 }
