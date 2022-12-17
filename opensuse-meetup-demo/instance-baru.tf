99340472b70516c73c5d4ad878f722fe49d0803c
takatux <takatux@localhost.localdomain>
2022-12-17 07:44:30 +0700

    add new instance

---
+++ opensuse-meetup-demo/instance-baru.tf
@@ -0,0 +1,16 @@
+module "instance" {
+  source          = "../modules/instance"
+  instance_name   = "opensuse-instance-baru-banget"
+  image           = "opensuse-leap-15-4-v20221201-x86-64"
+  tags            = var.tags
+  region          = var.region
+  env             = var.env
+  company         = var.company     
+  metadata_startup_script = <<SCRIPT
+    echo demo
+    sudo zypper -n update
+    sudo zypper install -y nginx
+    sudo systemctl start nginx
+    echo '<html> <head> <title>OpenSUSE ID Baru</title> </head> <body> <h1>Welcome to new Instance</h1> <p>This is a test server for static files.</p> <hr> <small>Powered by nginx and OpenSUSE Linux server</small> </body> </html>' | sudo tee /srv/www/htdocs/index.html
+    SCRIPT
+}
